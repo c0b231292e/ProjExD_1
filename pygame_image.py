7eb61856a5fe02ed713f9919702df8945718ea2a
C0B23129 <c0b231292e@edu.teu.ac.jp>
2024-09-24 15:46:12 +0900

    練習６：背景画像を動かす、間延び解消

--- pygame_image.py
+++ pygame_image.py
@@ -17,8 +17,8 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])#練習3
+        x = -(tmr%800)
+        screen.blit(bg_img, [x, 0])#練習3
         screen.blit(kk_img, [300, 200])#練習4
         pg.display.update()
         tmr += 1        
