34eabecbada70e209e708edaae115200f38a707d
C0B23129 <c0b231292e@edu.teu.ac.jp>
2024-09-24 16:25:41 +0900

    練習８：こうかとんの移動

--- pygame_image.py
+++ pygame_image.py
@@ -14,16 +14,27 @@ def main():
     kk_img = pg.image.load("fig/3.png")#練習2
     kk_img = pg.transform.flip(kk_img,True,False)#練習2
     kk_img = pg.transform.rotozoom(kk_img,10,1.0)#練習2
+    kk_rct = kk_img.get_rect()#練習8
+    kk_rct.center = 300,200#練習8
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()#練習８：キーの押下状態を取得
+        if key_lst[pg.K_UP]:#練習８：上矢印のとき
+            kk_rct.move_ip((0,-1))#練習８：縦座標を-1
+        if key_lst[pg.K_DOWN]:#練習８：下矢印のとき
+            kk_rct.move_ip((0,+1))#練習８：縦座標を+1
+        if key_lst[pg.K_RIGHT]:#練習８：右矢印のとき
+            kk_rct.move_ip((+1,0))#練習８：横座標を+1
+        if key_lst[pg.K_LEFT]:#練習８：左矢印のとき
+            kk_rct.move_ip((-1,0))#練習８：横座標を-1
         x = -(tmr%3200)#練習6#練習7
         screen.blit(bg_img, [x, 0])#練習3#練習6
         screen.blit(bg_img1, [x+1600, 0])#練習7
-        screen.blit(bg_img, [x+3200, 0])#練習3#練習6
-        screen.blit(bg_img1, [x+4800, 0])#練習3#練習6
-        screen.blit(kk_img, [300, 200])#練習4
+        screen.blit(bg_img, [x+3200, 0])#練習3#練習7
+        screen.blit(bg_img1, [x+4800, 0])#練習3#練習7
+        screen.blit(kk_img, kk_rct)#練習4#練習8
         pg.display.update()
         tmr += 1        
         clock.tick(200)
