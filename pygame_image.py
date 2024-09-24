7e13ce62e02690761968925e321ed693dcecd21a
C0B23129 <c0b231292e@edu.teu.ac.jp>
2024-09-24 15:30:46 +0900

    練習４：こうかとうん描画

--- pygame_image.py
+++ pygame_image.py
@@ -7,18 +7,19 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((800, 600))
+    screen = pg.display.set_mode((800, 600))#練習1
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    kk_img = pg.image.load("fig/3.png")
-    kk_img = pg.transform.flip(kk_img,True,False)
-    kk_img = pg.transform.rotozoom(kk_img,10,1.0)
+    kk_img = pg.image.load("fig/3.png")#練習2
+    kk_img = pg.transform.flip(kk_img,True,False)#練習2
+    kk_img = pg.transform.rotozoom(kk_img,10,1.0)#練習2
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [0, 0])#練習3
+        screen.blit(kk_img, [300, 200])#練習4
         pg.display.update()
         tmr += 1        
         clock.tick(10)
